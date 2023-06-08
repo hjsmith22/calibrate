4fbd3f56fe354b67bcc392734f869738edfd0d37
Hayden Smith <131565993+hjsmith22@users.noreply.github.com>
2023-06-08 14:43:09 -0400

    Create rejectedImgPoints.py

---
+++ rejectedImgPoints.py
@@ -0,0 +1,21 @@
+# plots the rejected points on a frame
+
+rejectedImgPoints_x = []
+        rejectedImgPoints_y = []
+
+        for i in range(len(rejectedImgPoints)):
+            temp_group = rejectedImgPoints[i]
+            for j in range(len(temp_group)):
+                temp_point = temp_group[j]
+                for k in range(len(temp_point)):
+                    temp_coord = temp_point[k]
+                    for l in range(len(temp_coord)):
+                        temp = temp_coord[l]
+                        if l == 0:
+                            rejectedImgPoints_x.append(temp)
+                        else:
+                            rejectedImgPoints_y.append(temp)
+
+        plt.imshow(gray)
+        plt.scatter(rejectedImgPoints_x, rejectedImgPoints_y)
+        plt.show()
