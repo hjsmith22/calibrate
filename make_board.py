d8dd3c46bceee9fca6d4792670daa370851f32d7
Hayden Smith <131565993+hjsmith22@users.noreply.github.com>
2023-06-20 12:45:41 -0400

    Update make_board.py

--- make_board.py
+++ make_board.py
@@ -1,6 +1,7 @@
 import numpy as np
 import cv2.aruco
 
+
 ARUCO_DICT = {
 	"DICT_4X4_50": cv2.aruco.DICT_4X4_50,
 	"DICT_4X4_100": cv2.aruco.DICT_4X4_100,
@@ -23,14 +24,16 @@ ARUCO_DICT = {
 	"DICT_APRILTAG_25h9": cv2.aruco.DICT_APRILTAG_25h9,
 	"DICT_APRILTAG_36h10": cv2.aruco.DICT_APRILTAG_36h10,
 	"DICT_APRILTAG_36h11": cv2.aruco.DICT_APRILTAG_36h11
+	# need an all black pattern
 }
 
+# takes marker id and returns the marker
 def id2marker(marker_id):
 	for key, val in aruco_dictionary.items():
 		if val == marker_id:
 			return key
 	return None
-
+# adds a white border 3 pixels wide around the marker: 6x6 -> 12x12
 def add_border(marker):
 	marker_with_border = np.ones((12, 12))
 	for i in range(12):
@@ -40,7 +43,7 @@ def add_border(marker):
 				val2 = marker[i - 3, j - 3]
 				marker_with_border[i, j] = marker[i - 3, j - 3]
 	return marker_with_border
-
+# charuco board: ids 00-39 written as their #, black is -1
 charuco = [
 		[0, -1, 1, -1, 2, -1, 3, -1, 4, -1],
 		[-1, 5, -1, 6, -1, 7, -1, 8, -1, 9],
@@ -49,9 +52,9 @@ charuco = [
 		[20, -1, 21, -1, 22, -1, 23, -1, 24, -1],
 		[-1, 25, -1, 26, -1, 27, -1, 28, -1, 29],
 		[30, -1, 31, -1, 32, -1, 33, -1, 34, -1],
-		[-1, 35, -1, 37, -1, 38, -1, 39, -1, 40]
+		[-1, 35, -1, 36, -1, 37, -1, 38, -1, 39]
 	] # -1 is black
-
+# black marker (12x12)
 black = [
 		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -66,7 +69,7 @@ black = [
 		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 	]
-
+# makes a board within the 6x6_250 aruco library
 def make_board(marker_size):
 	image_size = marker_size + 2  # * border_size
 	image = np.ones((image_size, image_size), dtype=np.uint8) * 255  # 255 creates a white background
@@ -99,7 +102,7 @@ aruco_dictionary = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_6X6_250)
 print("ArUCo type '{}' with ID '{}'".format(aruco_type, id))
 tag_size = 250
 tag = np.zeros((tag_size, tag_size, 1), dtype="uint8")
-cv2.aruco.drawMarker(arucoDict, id, tag_size, tag, 1)
+cv2.aruco.drawMarker(aruco_dictionary, id, tag_size, tag, 1)
 
 # save tag
 tag_name = "arucoMarkers/" + aruco_type + "_" + str(id) + ".png"
