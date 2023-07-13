649f50316933dbd98321f1d08f4824f423b3b5e3
Hayden Smith <131565993+hjsmith22@users.noreply.github.com>
2023-07-13 13:02:00 -0400

    Update rejectedImgPoints.py

--- rejectedImgPoints.py
+++ rejectedImgPoints.py
@@ -1,53 +1,51 @@
 # plots the rejected points on a frame
 
 rejectedImgPoints_x = []
-        rejectedImgPoints_y = []
+rejectedImgPoints_y = []
         framenum = 0
 
         vidnames = ['ridCalibraiton_box01_20230608_15-51-56_direct_700-1350-1-935.avi',
                     'ridCalibraiton_box01_20230608_15-51-56_leftmirror_1-470-270-920_flipped.avi',
                     'ridCalibraiton_box01_20230608_15-51-56_rightmirror_1570-2040-1-935_flipped.avi']
-        good_frames = [1, 2, 3]  # change later
+        vidname = 'ridCalibraiton_box01_20230608_15-51-56_direct_700-1350-1-935.avi'
+        good_frames = [260, 261, 262, 263, 264, 265, 266, 267, 268, 269]  # change later
 
-        for i in range(len(vidnames)):
-            vidname = vidnames[i]
-            for j in range(len(rejectedImgPoints)):
-                temp_group = rejectedImgPoints[j]
-                for k in range(len(temp_group)):
-                    temp_point = temp_group[k]
-                    for l in range(len(temp_point)):
-                        temp_coord = temp_point[l]
-                        for m in range(len(temp_coord)):
-                            temp = temp_coord[m]
-                            if m == 0:
-                                rejectedImgPoints_x.append(temp)
-                            else:
-                                rejectedImgPoints_y.append(temp)
-                    framenum += 1
-                    framenum_str = "{}".format(framenum)
+        for i in range(len(rejectedImgPoints)):
+            temp_group = rejectedImgPoints[i]
+            for j in range(len(temp_group)):
+                temp_point = temp_group[j]
+                for k in range(len(temp_point)):
+                    temp_coord = temp_point[k]
+                    for l in range(len(temp_coord)):  # makes coordinate
+                        temp = temp_coord[l]
+                        if l == 0:
+                            rejectedImgPoints_x.append(temp)
+                        else:
+                            rejectedImgPoints_y.append(temp)
+        framenum += 1
+        framenum_str = "{}".format(framenum)
+        plt.imshow(gray)
+        plt.scatter(rejectedImgPoints_x, rejectedImgPoints_y)
+        # plt.show()
+        filename = vidname.rsplit(".", 1)[0] + '_' + framenum_str
+        if framenum in good_frames:
+            image_path_out = r'C:\Users\haydenjs\Desktop\good_frames' + '\G' + filename + '.png'
+        else:
+            image_path_out = r'C:\Users\haydenjs\Desktop\bad_frames' + '\G' + filename + '.png'
 
-                plt.imshow(gray)
-                plt.scatter(rejectedImgPoints_x, rejectedImgPoints_y)
-                # plt.show()
-                filename = vidname.rsplit(".", 1)[0] + '_' + framenum_str
-                if framenum in good_frames:
-                    image_path_out = r'C:\Users\haydenjs\Desktop\good_frames' + '\G' + filename + '.png'
-                else:
-                    image_path_out = r'C:\Users\haydenjs\Desktop\bad_frames' + '\G' + filename + '.png'
+        data_path = r'C:\Users\haydenjs\Desktop\rejected_npys' + '\G' + filename + '.npy'
+        image_path_in = r'C:\Users\haydenjs\Desktop\rejected_pngs' + '\G' + filename + '.png'
 
-                data_path = r'C:\Users\haydenjs\Desktop\rejected_npys' + '\G' + filename + '.npy'
-                image_path_in = r'C:\Users\haydenjs\Desktop\rejected_pngs' + '\G' + filename + '.png'
+        np.save(data_path, gray)
+        data = np.load(data_path)
+        image = Image.fromarray(data)
+        image.save(image_path_in)
 
-                np.save(data_path, gray)
-                data = np.load(data_path)
-                image = Image.fromarray(data)
-                image.save(image_path_in)
+        image = Image.open(image_path_in)
+        image_array = np.array(image)
+        fig, ax = plt.subplots()
+        ax.imshow(image_array)
+        ax.scatter(rejectedImgPoints_x, rejectedImgPoints_y, color='red', marker='o')
 
-                image = Image.open(image_path_in)
-                image_array = np.array(image)
-                fig, ax = plt.subplots()
-                ax.imshow(image_array)
-                ax.scatter(rejectedImgPoints_x, rejectedImgPoints_y, color='red', marker='o')
-
-                plt.savefig(image_path_out)
-                plt.close(fig)
+        plt.savefig(image_path_out)
+        plt.close(fig)
