c40aa5e1c58955e5d11770181e114383602891e2
Hayden Smith <131565993+hjsmith22@users.noreply.github.com>
2023-07-17 15:57:42 -0400

    Update hflip.py

--- hflip.py
+++ hflip.py
@@ -1,9 +1,10 @@
-    if view_name == 'leftmirror' or 'rightmirror':
-        # flip the video we just wrote out horizontally, give it a new name with "_flip" at the end
-        # flipped_vid_name = (vid_name) + _flip
+    if view_name != 'direct':
+        # flip the video we just wrote out horizontally, give it a new name with "_flipped" at the end
+        # temporarily removes the video's file extension
+        flipped_vid_path_out = vid_path_out.rsplit(".", 1)[0] + '_flipped.avi'
         hflip_command = (
-            f"ffmpeg - i {vid_path_out} "
+            f"ffmpeg -i {vid_path_out} "
             f"-vf hflip "
-            f"-c:a {vid_path_out}"
+            f"-c:a copy {flipped_vid_path_out}"
         )
         subprocess.call(hflip_command, shell=True)
